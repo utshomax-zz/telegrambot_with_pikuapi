0bdd3261a2cff349533340599a010d58ec08e502
UTSAB UTSHO <utsab8008@gmail.com>
2020-08-02 19:38:30 +0530

    submit

--- commands.py
+++ commands.py
@@ -7,7 +7,7 @@ class command():
 
     def __init__(self, config):
         self.apikey = self.read_token_from_config_file(config)
-        self.base = "https://pikuapi.herokuapp.com/cnv/{}?".format(self.apikey)
+        self.base = "https://pikuapi.herokuapp.com/conv/{}?".format(self.apikey)
 
 
     def getReply(self, msg):
